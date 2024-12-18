9b08d4e9f88c94fe95e6f1325b0ed51a8e95065e
AnhTuan1678 <21020397@vnu.edu.vn>
2024-12-18 17:53:53 +0700

    ghép client với server

--- server/config/db.js
+++ server/config/db.js
@@ -1,13 +1,25 @@
 const mysql = require('mysql2/promise');
+require('dotenv').config({ path: './main.env' });
 
-const db = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'savenow',
-  port: 3306
+const config = {
+  host: process.env.MYSQL_SERVICE_HOST || process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.MYSQL_SERVICE_PORT || process.env.DB_PORT,
+};
+const db = mysql.createPool(config);
 
-});
-
+// Kiểm tra kết nối
+(async () => {
+  try {
+    const connection = await db.getConnection();
+    await connection.ping();
+    console.log(`Kết nối đến MySQL thành công: ${connection.config.database}`);
+    connection.release();
+  } catch (error) {
+    console.error(`Kết nối đến MySQL thất bại: ${error.message}`);
+  }
+})();
 
 module.exports = db;
--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
-const adminWarehouseRoutes = require('./routes/adminWarehouseRoutes')
-const adminRoutes = require('./routes/adminRoutes')
-const staffRoutes = require('./routes/staffRoutes')
-const statementRoutes = require('./routes/statementRoutes')
-const requestRoutes = require('./routes/requestRoutes')
-const logRoutes = require('./routes/logsRoutes')
+const adminWarehouseRoutes = require('./routes/adminWarehouseRoutes');
+const adminRoutes = require('./routes/adminRoutes');
+const staffRoutes = require('./routes/staffRoutes');
+const statementRoutes = require('./routes/statementRoutes');
+const requestRoutes = require('./routes/requestRoutes');
+const logRoutes = require('./routes/logsRoutes');
 const db = require('./config/db');
+require('dotenv').config({ path: './main.env' });
 
 const app = express();
 app.use(express.json());
@@ -21,9 +22,15 @@ app.use('/statement', statementRoutes);
 app.use('/adRequest', requestRoutes);
 app.use('/adLogs', logRoutes);
 
-
-app.listen(5001, () => {
-console.log('Server running on port 5001');
+// Giao diện
+app.use(express.static('build'));
+const CLIENT_PATH = 'build/index.html';
+app.get('*', (req, res) => {
+  res.sendFile(CLIENT_PATH, { root: __dirname });
 });
 
-
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  console.log(`http://localhost:${PORT}`);
+});
